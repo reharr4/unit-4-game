ef8cb98f40f24479e3c4757cae0967032a438714
Harrison, Rachel <reharr4@gmail.com>
2019-06-03 07:17:17 -0500

    added jumbotron resized crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,75 +1,79 @@
-// variables
-var wins= 0;
-var losses = 0;
-var playerScore = 0;
-var winningScore;
+$(document).ready(function () {
 
-// random number to reach for a win
-function makeWinningScore() {
-    winningScore = Math.floor(Math.random() * 101) + 19;
-    $("#points").text(winningScore);
-    console.log(winningScore);
+    // variables
+    var wins = 0;
+    var losses = 0;
+    var playerScore = 0;
+    var winningScore;
 
-}
+    // random number to reach for a win
+    function makeWinningScore() {
+        winningScore = Math.floor(Math.random() * 101) + 19;
+        $("#points").text(winningScore);
+        console.log(winningScore);
 
-// random values for crystals
-function crystalValue() {
-    oneValue = Math.floor(Math.random() * 11) + 1;
-    twoValue = Math.floor(Math.random() * 11) + 1;
-    threeValue = Math.floor(Math.random() * 11) + 1;
-    fourValue = Math.floor(Math.random() * 11) + 1;
-}
+    }
 
-// on.click function for crystals
-$("#orange").on("click", function () {
-    playerScore += oneValue;
-    winsLosses();
-    $("#yourScore").text(playerScore);
-});
+    // random values for crystals
+    function crystalValue() {
+        oneValue = Math.floor(Math.random() * 11) + 1;
+        twoValue = Math.floor(Math.random() * 11) + 1;
+        threeValue = Math.floor(Math.random() * 11) + 1;
+        fourValue = Math.floor(Math.random() * 11) + 1;
+    }
 
-$("#blue").on("click", function () {
-    playerScore += twoValue;
-    winsLosses();
-    $("#yourScore").text(playerScore);
-});
+    // on.click function for crystals
+    $("#orange").on("click", function () {
+        playerScore += oneValue;
+        winsLosses();
+        $("#yourScore").text(playerScore);
+    });
 
-$("#yellow").on("click", function () {
-    playerScore += threeValue;
-    winsLosses();
-    $("#yourScore").text(playerScore);
-});
+    $("#blue").on("click", function () {
+        playerScore += twoValue;
+        winsLosses();
+        $("#yourScore").text(playerScore);
+    });
 
-$("#pink").on("click", function () {
-    playerScore += fourValue;
-    winsLosses();
-    $("#yourScore").text(playerScore);
-});
+    $("#yellow").on("click", function () {
+        playerScore += threeValue;
+        winsLosses();
+        $("#yourScore").text(playerScore);
+    });
 
-// wins and losses
-function winsLosses() {
-    if (playerScore === winningScore) {
-        wins++;
+    $("#pink").on("click", function () {
+        playerScore += fourValue;
+        winsLosses();
+        $("#yourScore").text(playerScore);
+    });
+
+    // wins and losses
+    function winsLosses() {
+        if (playerScore === winningScore) {
+            wins++;
+            $("#win").text(wins);
+            reset();
+        }
+        else if (playerScore > winningScore) {
+            losses++;
+            $("#loss").text(losses);
+            reset();
+        }
+    }
+
+    // reset function
+    function reset() {
+        playerScore = 0;
+        $("#yourScore").text(playerScore);
+
+        makeWinningScore();
+        crystalValue();
+
         $("#win").text(wins);
-        reset();
-    }
-    else if (playerScore > winningScore) {
-        losses++;
         $("#loss").text(losses);
-        reset();
+
     }
-}
 
-// reset function
-function reset() {
-    playerScore = 0;
-    $("#yourScore").text(playerScore);
+    reset();
 
-    makeWinningScore();
-    crystalValue();
-
-    $("#win").text(wins);
-    $("#loss").text(losses);
-
-}
-
-reset();
+});
